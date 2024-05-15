77062cdae0e7466ac4279f38043f15870490a9ff
upendra <upendrasaisrinivas@gmail.com>
2024-05-15 13:41:02 +0530

    Updated App.js

--- App.js
+++ App.js
@@ -1,28 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "Heading 1"
-  );
-  const heading2 = React.createElement(
-    "h2",
-    {
-      id: "title",
-    },
-    "Heading 2"
-  );
-  const container = React.createElement(
-    "div",
-    {
-      id: "container",
-    },
-    [heading, heading2]
-  );
-  console.log(heading);
+const container = <h1 id="container">This is JSX first code</h1>
   const root = ReactDOM.createRoot(document.getElementById("root"));
   //passing a react element inside the root
   //async defer
